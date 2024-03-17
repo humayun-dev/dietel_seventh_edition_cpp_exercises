4e1e53ed4da840f8c0f83bc9803945801b951408
humayun-dev <humayun.devv@gmail.com>
2024-03-18 00:46:53 +0500

    testing.h file updated for linear search

--- chapterseven/main.cpp
+++ chapterseven/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "testing.h"
 
 using namespace::std;
 
 int main(int argc, char** argv) {
+	linear_searching();
 	return 0;
 }
---
+++ chapterseven/testing.h
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace::std;
+
+/*
+// implementation of the linear search
+int search(int [],int);		// function prototype
+
+void linear_searching(){
+	
+	int array[100],i,element,result;
+	
+	// create some data for the array
+	for(i = 0; i < 100; i++){
+		array[i] = i + 5;
+	}	
+	
+	// display the created elements in the array
+	for(i = 0; i < 100; i++){
+		cout<<array[i]<<" ";
+		if(i != 0 && i % 20 == 0){
+			cout<<endl;
+		}
+	}
+	cout<<endl;
+	// user input for the searched element
+	cout<<"Please enter the element for the linear search: ";
+	cin>>element;
+	
+	result = search(array,element);
+	if(result != -1){
+		cout<<"The element "<<element<<" found at "<<result<<" index number"<<endl;
+	}else{
+		cout<<"Element not found in the array";
+	}
+}
+// search function will traverse in the array for searching of the user input
+int search(int array[],int key){
+	for(int i = 0; i < 100; i++){
+		if(array[i] == key){
+			return i;
+		}
+	}
+	return -1;
+}
+*/
