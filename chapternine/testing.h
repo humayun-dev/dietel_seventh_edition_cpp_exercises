4f9501c009a1c01c9cec7f7d16b4b6a8a713e50d
humayun-dev <humayun.devv@gmail.com>
2024-03-25 06:49:22 +0500

    macro define

--- chapternine/testing.h
+++ chapternine/testing.h
@@ -1,6 +1,8 @@
 /*	Program: Chapter 9 header file
 	Author: Muhammad Humayun Khan
 */
+#ifndef UTILITYFUNCTION
+#define UTILITYFUNCTION
 
 class testing{
 	private:
@@ -10,5 +12,7 @@ class testing{
 		testing(int);		// by default contructor
 		void addition();	// public member function
 };
+
+#endif
 
 
